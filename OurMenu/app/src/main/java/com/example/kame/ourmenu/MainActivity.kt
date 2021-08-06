c261d0e065fa27bac38d3bc6908cf4dcab945d92
kameoman <spiderman.935963@gmail.com>
2021-08-06 15:59:54 +0900

    app-menu-187

--- OurMenu/app/src/main/java/com/example/kame/ourmenu/MainActivity.kt
+++ OurMenu/app/src/main/java/com/example/kame/ourmenu/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kame.ourmenu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.home -> {
+                menuImage.setImageResource(R.drawable.cook)
+                menuText.text = ""
+                return true
+            }
+            R.id.greencurry -> {
+                menuImage.setImageResource(R.drawable.greencurry)
+                menuText.text = getString(R.string.greencurry_text)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
