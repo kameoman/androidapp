fd7f857cda65a6a404d59fbbf703c43eef62ffab
kameoman <spiderman.935963@gmail.com>
2021-08-06 16:22:11 +0900

    app-menu-188

--- OurMenu/app/src/main/java/com/example/kame/ourmenu/MainActivity.kt
+++ OurMenu/app/src/main/java/com/example/kame/ourmenu/MainActivity.kt
@@ -29,6 +29,58 @@ class MainActivity : AppCompatActivity() {
                 menuText.text = getString(R.string.greencurry_text)
                 return true
             }
+            R.id.beefcurry -> {
+                menuImage.setImageResource(R.drawable.beefcurry)
+                menuText.text = getString(R.string.beefcurry_text)
+                return true
+            }
+            R.id.tamoricurry -> {
+                menuImage.setImageResource(R.drawable.tamoricurry)
+                menuText.text = getString(R.string.tamoricurry_text)
+                return true
+            }
+            R.id.katsucurry -> {
+                menuImage.setImageResource(R.drawable.katsucurry)
+                menuText.text = getString(R.string.katsucurry_text)
+                return true
+            }
+            R.id.soba1 -> {
+                menuImage.setImageResource(R.drawable.soba1)
+                menuText.text = getString(R.string.soba1_text)
+                return true
+            }
+            R.id.soba2 -> {
+                menuImage.setImageResource(R.drawable.soba2)
+                menuText.text = getString(R.string.soba2_text)
+                return true
+            }
+            R.id.ramen1 -> {
+                menuImage.setImageResource(R.drawable.ramen1)
+                menuText.text = getString(R.string.ramen1_text)
+                return true
+            }
+            R.id.ramen2 -> {
+                menuImage.setImageResource(R.drawable.ramen2)
+                menuText.text = getString(R.string.ramen2_text)
+                return true
+            }
+            R.id.osyarenabe -> {
+                menuImage.setImageResource(R.drawable.nabeyaki)
+                menuText.text = getString(R.string.nabeyaki_text)
+                return true
+            }
+            R.id.udon -> {
+                menuImage.setImageResource(R.drawable.udon)
+                menuText.text = getString(R.string.udon_text)
+                return true
+            }
+            R.id.hiyashi -> {
+                menuImage.setImageResource(R.drawable.hiyashi)
+                menuText.text = getString(R.string.hiyashi_text)
+                return true
+            }
+
+
         }
         return super.onOptionsItemSelected(item)
     }
