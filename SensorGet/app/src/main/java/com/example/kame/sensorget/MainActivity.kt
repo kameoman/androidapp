c761055b57cd78f7678dc9335ccf3e3439bf84f4
kameoman <spiderman.935963@gmail.com>
2021-08-08 18:43:10 +0900

    app-menu-201

--- SensorGet/app/src/main/java/com/example/kame/sensorget/MainActivity.kt
+++ SensorGet/app/src/main/java/com/example/kame/sensorget/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.kame.sensorget
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var buffer = StringBuilder()
+
+        var sensorManager: SensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sList: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for (sensor: Sensor in sList) {
+            //Log.v(tag, sensor.type.toString())
+            //Log.v(tag, "${sensor.type.toString()},${sensor.name},${sensor.vendor}")
+            //buffer.append("${sensor.type},${sensor.name},${sensor.vendor}\n")
+            //Log.v(tag, "${sensor.stringType},${sensor.name},${sensor.vendor}")         // API Level >=  20
+            buffer.append("${sensor.stringType},${sensor.name},${sensor.vendor}\n")
+        }
+        txt01.text = buffer.toString()
+
+
     }
 }
